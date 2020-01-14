a461fc9a8c3508cd039eca8bd5596f00abe849ce
Roma <vakulka95@gmail.com>
2020-01-14 13:00:22 +0200

    My test work was written on clear HTML & CSS

---
+++ app.js
@@ -0,0 +1,32 @@
+$(function(){
+
+
+    /*  Fixed header  */
+
+
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+
+	$(window).on("scroll load resize", function() {
+		let introH = intro.innerHeight();
+		scrollPos = $(this).scrollTop();
+
+		if(scrollPos > introH) {
+			header.addClass("fixed");
+		}else{
+			header.removeClass("fixed");
+		}
+	})
+
+	/*  Smooth scroll  */
+
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		let elementId = $(this).data("scroll");
+
+		console.log(elementId);
+	})
+});
